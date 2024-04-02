08a20d69b6cf5edbafe7827810919ebb8dd93b38
Anish Kumbhar <anish.kumbhar04@gmail.com>
2024-04-03 04:48:11 +0530

    initial commit

---
+++ src/network/utils/iana-IEEE-802-numbers.h
@@ -0,0 +1,5 @@
+#include <stdint.h>
+
+namespace ns3 {
+    const uint16_t IEEE_802_IPV6 = 0x86DD;
+}
---
+++ src/network/utils/iana-ppp-dll-numbers.h
@@ -0,0 +1,5 @@
+#include <stdint.h>
+
+namespace ns3 {
+    const uint16_t PPP_DLL_IPV6 = 0x0057;
+}
